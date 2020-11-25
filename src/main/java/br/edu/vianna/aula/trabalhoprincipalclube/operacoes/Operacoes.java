f9b7cd3e03b1a6fab8eee169901b36303532ccb9
rafaelferreira01 <rafael.ferreira@viannasempre.com.br>
2020-11-25 01:03:20 -0300

    criada classe Operacoes e Login adicionadas varias images em resource

---
+++ src/main/java/br/edu/vianna/aula/trabalhoprincipalclube/operacoes/Operacoes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.vianna.aula.trabalhoprincipalclube.operacoes;
+
+/**
+ *
+ * @author suporte
+ */
+public class Operacoes {
+    
+
+    public static String verificaLogin(String login, String senha){
+        
+        if(login.equals("admin") && senha.equals("123")){
+            return "Admin";
+        }else{
+            return null;
+        }
+    }
+}
