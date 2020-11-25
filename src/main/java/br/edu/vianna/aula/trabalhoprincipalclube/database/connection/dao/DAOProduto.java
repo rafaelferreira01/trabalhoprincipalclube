f58bda57f1a2a9a9cbc7b2a1afec1ec71bbf16e3
Leunardu <leonardo.castilho@viannasempre.com.br>
2020-11-24 23:52:57 -0300

    DAOProduto feita!

--- src/main/java/br/edu/vianna/aula/trabalhoprincipalclube/database/connection/dao/DAOProduto.java
+++ src/main/java/br/edu/vianna/aula/trabalhoprincipalclube/database/connection/dao/DAOProduto.java
@@ -5,10 +5,109 @@
  */
 package br.edu.vianna.aula.trabalhoprincipalclube.database.connection.dao;
 
+import br.edu.vianna.aula.trabalhoprincipalclube.database.connection.ConnectionClube;
+import br.edu.vianna.aula.trabalhoprincipalclube.enums.ETipoProduto;
+import br.edu.vianna.aula.trabalhoprincipalclube.model.subclass.Produto;
+import br.edu.vianna.aula.trabalhoprincipalclube.usuario.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author suporte
  */
-public class DAOProduto {
+public class DAOProduto implements IDaoGenerics <Produto, Integer> {
+
+    @Override
+    public void inserir(Produto p) throws ClassNotFoundException, SQLException {
+         Connection c = ConnectionClube.getConnection();
+        
+        String comando = "INSERT INTO produto (tipo_produto, descricao, valor_produto) "
+                + "VALUES (?,?,?);";
+        
+        
+
+        PreparedStatement prepara  = c.prepareStatement(comando);
+        
+        prepara.setObject(1, p.getTipo());
+        prepara.setString(2, p.getDescricao());
+        prepara.setDouble(3, p.getValor());
+
+        prepara.executeUpdate();
+    }
     
+
+    @Override
+    public void alterar(Produto p) throws ClassNotFoundException, SQLException {
+        Connection c = ConnectionClube.getConnection();
+        
+        String comando = "UPDATE produto SET "
+                + "tipo_produto = ?, descricao = ?, valor_produto = ? "
+                + "WHERE id_produto = ?;";
+        
+        PreparedStatement prepara  = c.prepareStatement(comando);
+        
+        prepara.setObject(1, p.getTipo());
+        prepara.setString(2, p.getDescricao());
+        prepara.setDouble(3, p.getValor());
+        prepara.setInt(4, p.getId());
+        
+        prepara.executeUpdate();
+    }
+
+    @Override
+    public void apagar(Produto u) throws ClassNotFoundException, SQLException {
+        Connection c = ConnectionClube.getConnection();
+        String comando = "DELETE FROM produto "
+                + "WHERE id_produto = ?;";
+        PreparedStatement prepara  = c.prepareStatement(comando);
+        prepara.setInt(1, u.getId());
+        prepara.executeUpdate();
+    }
+
+    @Override
+    public Produto buscarPorId(Integer i) throws ClassNotFoundException, SQLException {
+        Connection c = ConnectionClube.getConnection();
+        
+        String comando = "SELECT * FROM produto "
+                + "WHERE id_produto = ?;";
+        PreparedStatement prepara  = c.prepareStatement(comando);
+        prepara.setInt(1, i);
+         
+        Produto produto = null;
+        ResultSet rs = prepara.executeQuery();//objeto ResultSet recebe todos os elementos da tabela buscada
+        
+        while(rs.next()) {
+            produto = new Produto(rs.getInt("id_produto"), ETipoProduto.valueOf(rs.getString("tipo_produto")), 
+            rs.getString("descricao"), rs.getDouble("valor"));
+        }
+        return produto;  
+      
+    } 
+
+    @Override
+    public ArrayList<Produto> buscarTodos() throws ClassNotFoundException, SQLException {
+        Connection c = ConnectionClube.getConnection();
+        String comando = "SELECT * FROM produto;";
+        PreparedStatement prepara  = c.prepareStatement(comando);
+        
+        ArrayList<Produto> lista = new ArrayList<>();
+        ResultSet rs = prepara.executeQuery();
+        
+        while(rs.next()) {
+            Produto produto = new Produto (rs.getInt("id_produto"), ETipoProduto.valueOf(rs.getString("tipo_produto")), 
+            rs.getString("descricao"), rs.getDouble("valor"));
+            lista.add(produto);
+        }
+        return lista;   
+    }
+
+    @Override
+    public int count() throws ClassNotFoundException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
